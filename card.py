051904374563d5ab0e57363cced0be7eee46768d
g-lancer <g4reg@pisem.net>
2014-03-25 13:23:53 +0400

    creating a deck inside a card v1

--- card.py
+++ card.py
@@ -1,5 +1,7 @@
 __author__ = 'Gl'
 
+import random
+
 class BjCard():
     suits = ['clubs', 'diamonds', 'hearts', 'spades']
     names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
@@ -12,6 +14,17 @@ class BjCard():
     def print(self):
         print(self.names[self.name], 'of ', self.suits[self.suit],', value ', self.values[self.name])
         return
+
+    def createdeck(self, decktype, numberofdecks):
+        deck = []
+        if decktype == 1:
+            for i in range(len(self.suits)):
+                for j in range(4,len(self.names)):
+                    c1 = self.BjCard(i,j)
+                    deck = deck.append(c1)
+        deck = deck * numberofdecks
+        random.shuffle(deck)
+        return deck
 
 
 
--- main.py
+++ main.py
@@ -8,9 +8,18 @@ class BlackjackTest(unittest.TestCase):
 #    def test1(self):
 #        self.failUnless(False)
 
-    def test_card_value_01(self):
+    def test_cardvalue01(self):
         bjcard = card.BjCard(0,1)
         self.failUnless(bjcard.value() == 3)
+
+    def testdecknorepeats(self):
+        d1 = card.BjCard.createdeck(1,1)
+        d1.sort()
+        hasdoubles = False
+        for i in range(1,len(d1)):
+            hasdoubles = hasdoubles and d1[i] == d1[i-1]
+        self.failIf(hasdoubles)
+
 
 def main():
     unittest.main()
